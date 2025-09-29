e1a128d7d34321d49efc642c33524e1424f404b1
Adam Miller-Kirk <adam_miller-kirk@hotmail.com>
2025-09-29 12:27:44 +0100

    feature calandar implemented at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Calendar from "@/components/calendar";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <Calendar />
+    </>
   );
 }
